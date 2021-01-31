9931c0fc8d8e443cd0a45a013673da6cd2d84f78
Vincent Lee <vincent.lee@outlook.com>
2021-01-30 21:08:01 -0600

    initial

---
+++ evaluator.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require racket/sandbox)
+(provide run)
+
+(define (format-response value stderr)
+  (format "~a~a"
+          (if (void? value) "" value)
+          (if (non-empty-string? stderr)
+              (string-append "\n:warning: stderr:\n" stderr)
+              "")))
+
+(define (test) (random 5))
+
+(define (init-evaluator text)
+  (parameterize ([sandbox-output #f]
+                 [sandbox-error-output 'string]
+                 [sandbox-propagate-exceptions #f])
+    (let ([evaluator (make-evaluator 'racket)])
+      (evaluator `(define message-contents ,text))
+      evaluator)))
+
+(define (run code text)
+  (let* ((evaluator (init-evaluator text))
+         (result (evaluator code))
+         (stderr (get-error-output evaluator)))
+    (kill-evaluator evaluator)
+    (format-response result stderr)))
---
+++ r16.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+(require (prefix-in rc: racket-cord))
+(require (prefix-in http: racket-cord/http))
+(require (prefix-in ev: "evaluator.rkt"))
+
+(define (strip-trim msg prefix)
+  (string-trim (substring msg (string-length prefix))))
+
+(define (message-from-bot? message)
+  (and (not (null? (rc:message-author message)))
+       (not (null? (rc:user-bot (rc:message-author message))))))
+
+(define (strip-backticks code)
+  (string-trim code "`" #:repeat? #t))
+
+(define (run-snippet client message code)
+  (let ([code (strip-backticks code)]
+        [text (rc:message-content message)])
+    (ev:run code text)))
+        
+(define (register-trick client message rst)
+  (~a "Registering " message))
+
+(define (call-trick client message name)
+  (~a "Calling " name))
+
+(define (show-trick client message name)
+  (~a "Showing " name))
+
+(define command-table (list (cons "eval" run-snippet)
+                            (cons "register" register-trick)
+                            (cons "call" call-trick)
+                            (cons "show" show-trick)))
+
+(define (dispatch-command client message text)
+  (ormap (lambda (pair)
+           (let ([cmdname (car pair)]
+                 [func (cdr pair)])
+             (and (string-prefix? text cmdname)
+                  (func client message (strip-trim text cmdname)))))
+         command-table))
+
+(define prefix "!rkt") 
+
+(define (message-received client message)
+  (let ([content (string-trim (rc:message-content message))]
+        [channel (rc:message-channel-id message)])
+    (if (and (not (message-from-bot? message))
+             (string-prefix? content prefix))
+        (let ([response (dispatch-command client message (strip-trim (rc:message-content message) prefix))])
+          (http:create-message client channel response))
+        #f)))
+
+(define (init-client token)
+  (let ([client (rc:make-client token #:auto-shard #t)])
+    (rc:on-event 'message-create client message-received)
+    client))
+
+(define (main)
+  (let ([token (getenv "BOT_TOKEN")])
+    (if (not token)
+        (error "No token provided")
+        (rc:start-client (init-client token)))))
+
+(module* main #f
+  (main))
