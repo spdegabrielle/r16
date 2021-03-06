dc85b0e9dea671e44399597068555f2717eccf8d
Vincent Lee <vincent@vincent-lee.net>
2021-03-06 12:37:55 -0600

    Start on presentation

---
+++ presentation/main.rkt
@@ -0,0 +1,11 @@
+#lang slideshow/widescreen
+
+(require slideshow/text)
+
+(slide
+ (big (t "R16"))
+ (t "A Discord Bot for Community-Driven, Interactive Code Evaluation")
+ (blank-line)
+ (t "Racketfest Amateur Night")
+ (t "March 27, 2021")
+ (t "Presented by: Vincent Lee (williewillus), Alwinfy, Benedek Szilvasby (Eutro)"))
