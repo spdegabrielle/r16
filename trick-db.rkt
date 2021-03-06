f96ec1436e0cdcec4df21a3fdf4210556cb3e1d5
Vincent Lee <vincent@vincent-lee.net>
2021-03-05 23:31:55 -0600

    Tests for trickdb

--- trick-db.rkt
+++ trick-db.rkt
@@ -2,7 +2,6 @@
 
 (require
   racket/contract
-  racket/hash
   racket/serialize)
 
 (define saveable-trick? serializable?)
@@ -33,12 +32,12 @@
 (define (serialize-db db)
   (serialize (hash->list (trickdb-data db))))
 
-(define (try-read-db filename default)
-  (with-handlers ([exn:fail? (lambda (e) (displayln e (current-error-port)) (default))])
-    (make-hash (deserialize (read (open-input-file filename))))))
+(define (try-read-db port)
+  (make-hash (deserialize (read port))))
 
 (define (make-trickdb filename)
-  (let ([data (try-read-db filename make-hash)])
+  (let ([data (with-handlers ([exn:fail? (lambda (e) (log-error (~a e)) (make-hash))])
+                (call-with-input-file* filename try-read-db))])
     (trickdb
      data
      filename
@@ -112,3 +111,73 @@
               (set-trickdb-dirty! db #f)
               (log-debug "db flushed")
               #t))))))
+
+(module* test #f
+  (require rackunit)
+
+  (serializable-struct
+   fake-trick
+   (value)
+   #:mutable
+   #:transparent)
+
+  (test-case "CRUD Smoke test"
+    (define context-id 1)
+    (define trick-id "trick1")
+    (define db (make-trickdb "/tmp/dummy")) ; we aren't reading/writing this db so this doesn't matter
+    (check-equal? (list-tricks db context-id) null)
+    (check-equal? (all-tricks db context-id) null)
+
+    (check-true (add-trick! db context-id trick-id (thunk (fake-trick "foo"))))
+    (check-equal? (fake-trick-value (get-trick db context-id trick-id)) "foo")
+
+    (check-equal? (length (list-tricks db context-id)) 1)
+    (check-equal? (length (all-tricks db context-id)) 1)
+
+    (check-true (update-trick! db context-id trick-id
+                               (const (fake-trick "bar"))
+                               (const #t)))
+    (check-equal? (fake-trick-value (get-trick db context-id trick-id)) "bar")
+
+    (check-true (remove-trick! db context-id trick-id (const #t)))
+    (check-equal? (list-tricks db context-id) null)
+    (check-equal? (all-tricks db context-id) null))
+
+  (test-case "Duplicate and Missing Test"
+    (define context-id 1)
+    (define trick-id "trick1")
+    (define db (make-trickdb "/tmp/dummy")) ; we aren't reading/writing this db so this doesn't matter
+
+    (check-true (add-trick! db context-id trick-id (thunk (fake-trick "foo"))))
+    (check-false (add-trick! db context-id trick-id (thunk (fake-trick "foo2"))))
+
+    (check-false (update-trick! db context-id "nonexistent-id" values (const #t))
+    (check-false (remove-trick! db context-id "nonexistent-id" (const #t)))))
+
+  (test-case "Context Separation Test"
+    (define context-id-1 1)
+    (define context-id-2 2)
+    (define trick-id "trick1")
+    (define db (make-trickdb "/tmp/dummy")) ; we aren't reading/writing this db so this doesn't matter
+
+    (check-true (add-trick! db context-id-1 trick-id (thunk (fake-trick "foo"))))
+    (check-true (add-trick! db context-id-2 trick-id (thunk (fake-trick "bar"))))
+
+    (check-equal? (fake-trick-value (get-trick db context-id-1 trick-id)) "foo")
+    (check-equal? (fake-trick-value (get-trick db context-id-2 trick-id)) "bar"))
+
+  (test-case "Saving Test"
+    (define context-id-1 1)
+    (define context-id-2 2)
+    (define trick-id "trick1")
+    (define path (make-temporary-file))
+    (after
+     (let ([db (make-trickdb path)])
+       (check-true (add-trick! db context-id-1 trick-id (thunk (fake-trick "foo"))))
+       (check-true (add-trick! db context-id-2 trick-id (thunk (fake-trick "bar"))))
+       (check-true (commit-db! db)))
+     (let ([db (make-trickdb path)])
+       (check-equal? (fake-trick-value (get-trick db context-id-1 trick-id)) "foo")
+       (check-equal? (fake-trick-value (get-trick db context-id-2 trick-id)) "bar"))
+
+     (delete-file path))))
